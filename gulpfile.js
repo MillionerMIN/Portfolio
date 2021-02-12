28ec333baf7a3729e409c1e28b8f08adbc255405
Vladimir <vladimirlyn@gmail.com>
2021-02-12 14:59:17 +0300

    step 2

---
+++ gulpfile.js
@@ -0,0 +1,72 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync');
+const sass = require('gulp-sass');
+const rename = require("gulp-rename");
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const imagemin = require('gulp-imagemin');
+const { on } = require('events');
+
+// Static server
+gulp.task('server', function () {
+  browserSync.init({
+    server: {
+      baseDir: "src"
+    }
+  });
+});
+
+gulp.task('styles', function () {
+  return gulp.src('src/sass/*.+(sass|scss)')
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(rename({
+      prefix: "",
+      suffix: ".min"
+    }))
+    .pipe(autoprefixer({
+      cascade: false
+    }))
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(gulp.dest('src/css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('watch', function () {
+  gulp.watch('src/sass/*.+(sass|scss)', gulp.parallel("styles"));
+  gulp.watch('src/*.html').on('change', browserSync.reload);
+});
+
+gulp.task('html', function () {
+  return gulp.src('src/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest("dist/"));
+});
+
+gulp.task('scripts', function () {
+  return gulp.src('src/js/**/*.js')
+        .pipe(gulp.dest("dist/js"));
+});
+
+gulp.task('fonts', function () {
+  return gulp.src('src/fonts/**/*')
+        .pipe(gulp.dest("dist/fonts"));
+});
+
+gulp.task('icons', function () {
+  return gulp.src('src/icons/**/*')
+        .pipe(gulp.dest("dist/icons"));
+});
+
+gulp.task('mailer', function () {
+  return gulp.src('src/mailer/**/*')
+        .pipe(gulp.dest("dist/mailer"));
+});
+
+gulp.task('images', function () {
+  gulp.src('src/img/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'images'));
+
