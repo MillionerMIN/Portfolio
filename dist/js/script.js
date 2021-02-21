bb202abe9ff8f455565d33af149a854235c08280
Vladimir <vladimirlyn@gmail.com>
2021-02-21 18:09:03 +0300

    Step 6

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
-    
+
     const hamburger = document.querySelector('.hamburger'),
         menu = document.querySelector('.menu'),
         closeElem = document.querySelector('.menu__close');
@@ -12,6 +12,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
     closeElem.addEventListener('click', () => {
         menu.classList.remove('active');
+    });
+
+    const procent = document.querySelectorAll(".work__progress-procent"),
+        lines = document.querySelectorAll(".work__progress-line span");
+
+    procent.forEach((item, i) => {
+        lines[i].style.width = item.innerHTML;
     });
 
 });
